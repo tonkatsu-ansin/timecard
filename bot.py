98e95f49b0e9ccaf212a002a7497034c4989b13e
tori <tigeriwamura@gmail.com>
2020-04-20 21:04:10 +0900

    [change]botの実装をbotクラスに変更
    [add] pingコマンドを実装

--- bot.py
+++ bot.py
@@ -1,21 +1,22 @@
 import discord
+from discord.ext import commands
 from datetime import datetime,timedelta,timezone
 from os import getenv
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='$')
 
 def get_destination():
-    Channel = client.get_channel(int(getenv('CHANNEL_ID')))
+    Channel = bot.get_channel(int(getenv('CHANNEL_ID')))
     return Channel
 
-@client.event
+@bot.event
 async def on_ready():
     destination = get_destination()
     await destination.send("Timecard is ready!")
 
-@client.event
+@bot.event
 async def on_voice_state_update(Member,before,after):
-    if (before.channel != after.channel) and Member.name != client.user.name:
+    if (before.channel != after.channel) and Member.name != bot.user.name:
         destination = get_destination()
         now = datetime.now(timezone(timedelta(hours=9)))
         date = str(now.strftime("%Y/%m/%d %H:%M:%S")+" ")
@@ -27,4 +28,13 @@ async def on_voice_state_update(Member,before,after):
         elif(after.channel != before.channel):
             await destination.send(date + Member.name + " moved to " + str(after.channel) + " from " + str(before.channel))
 
-client.run(getenv('BOT_TOKEN'));
+@bot.event
+async def on_command_error(ctx,error):
+    pass #botのpingのクールダウン中にカスタムステータスに設定したい
+
+@bot.command()
+@commands.cooldown(1,180,commands.BucketType.channel)
+async def ping(ctx):
+    await ctx.send("pong")
+
+bot.run(getenv('BOT_TOKEN'))
